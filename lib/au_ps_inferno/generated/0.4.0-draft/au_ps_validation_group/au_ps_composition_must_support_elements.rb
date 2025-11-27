d476d8b14a58a88171135b953f20670f5519e1de
Pavel Rozhkov <prozskov@gmail.com>
2025-11-27 15:17:10 +0100

    Refactor Composition must-support element tests
    - Centralized mandatory and optional must-support element definitions into constants.
    - Simplified test logic using reusable `execute_statistics` helper method.
    - Improved clarity and consistency in processing mandatory and optional elements.

--- lib/au_ps_inferno/generated/0.4.0-draft/au_ps_validation_group/au_ps_composition_must_support_elements.rb
+++ lib/au_ps_inferno/generated/0.4.0-draft/au_ps_validation_group/au_ps_composition_must_support_elements.rb
@@ -2,6 +2,27 @@
 
 require 'jsonpath'
 
+MANDATORY_MS_ELEMENTS = [
+  {:expression => "$.status", :label => "status"},
+  {:expression => "$.type", :label => "type"},
+  {:expression => "$.subject.reference", :label => "subject.reference"},
+  {:expression => "$.date", :label => "date"},
+  {:expression => "$.author[0]", :label => "author"},
+  {:expression => "$.title", :label => "title"}
+].freeze
+
+OPTIONAL_MS_ELEMENTS = [
+  {:expression => "$.text", :label => "text"},
+  {:expression => "$.identifier", :label => "identifier"},
+  {:expression => "$.attester", :label => "asserter"},
+  {:expression => "$.attester.mode", :label => "asserter.mode"},
+  {:expression => "$.attester.time", :label => "asserter.time"},
+  {:expression => "$.attester.party", :label => "asserter.party"},
+  {:expression => "$.custodian", :label => "custodian"},
+  {:expression => "$.event.code.coding.code", :label => "event"},
+  {:expression => "$.event.code", :label => "event.code"},
+  {:expression => "$.event.period", :label => "event.period"}].freeze
+
 module AUPSTestKit
   class AUPSCompositionMUSTSUPPORTElements < Inferno::Test
     title 'Composition has must-support elements'
@@ -15,52 +36,27 @@ module AUPSTestKit
         warning message
       end
     end
+    
+    def execute_statistics(json_data, json_path_expression, message_base, humanized_name)
+      data_value = JsonPath.on(json_data, json_path_expression).first.present?
+      show_message("#{message_base}: #{humanized_name}: #{data_value}", data_value)
+    end
 
     def composition_mandatory_ms_elements_info
-      data_for_testing = scratch[:ips_bundle_resource].to_json
-      composition_resource = JsonPath.on(data_for_testing, '$.entry[?(@.resource.resourceType == "Composition")].resource').first
-
-      status = JsonPath.on(composition_resource, '$.status').first.present?
-      type = JsonPath.on(composition_resource, '$.type').first.present?
-      subject_reference = JsonPath.on(composition_resource, '$.subject.reference').first.present?
-      date = JsonPath.on(composition_resource, '$.date').first.present?
-      author = JsonPath.on(composition_resource, '$.author[0]').first.present?
-      title = JsonPath.on(composition_resource, '$.title').first.present?
-      section_title = JsonPath.on(composition_resource, '$.section.*').length == JsonPath.on(composition_resource, '$.section.*.title').length
-      section_text = JsonPath.on(composition_resource, '$.section.*').length == JsonPath.on(composition_resource, '$.section.*.text').length
+      composition_resource = JsonPath.on(scratch[:ips_bundle_resource].to_json,
+                                         '$.entry[?(@.resource.resourceType == "Composition")].resource').first
       message_base = "List of Mandatory Must Support elements populated"
 
-      show_message("#{message_base}: status: #{status}", status)
-      show_message("#{message_base}: type: #{type}", type)
-      show_message("#{message_base}: subject.reference: #{subject_reference}", subject_reference)
-      show_message("#{message_base}: date: #{date}", date)
-      show_message("#{message_base}: author: #{author}", author)
-      show_message("#{message_base}: title: #{title}", title)
+      MANDATORY_MS_ELEMENTS.each { |element| execute_statistics(composition_resource, element[:expression], message_base, element[:label]) }
+      section_title = JsonPath.on(composition_resource, '$.section.*').length == JsonPath.on(composition_resource, '$.section.*.title').length
+      section_text = JsonPath.on(composition_resource, '$.section.*').length == JsonPath.on(composition_resource, '$.section.*.text').length
       show_message("#{message_base}: section.title: #{section_title}", section_title)
       show_message("#{message_base}: section.text: #{section_text}", section_text)
 
-      text = JsonPath.on(composition_resource, '$.text').first.present?
-      identifier = JsonPath.on(composition_resource, '$.identifier').first.present?
-      asserter = JsonPath.on(composition_resource, '$.attester').first.present?
-      asserter_mode = JsonPath.on(composition_resource, '$.attester.mode').first.present?
-      asserter_time = JsonPath.on(composition_resource, '$.attester.time').first.present?
-      asserter_party = JsonPath.on(composition_resource, '$.attester.party').first.present?
-      custodian = JsonPath.on(composition_resource, '$.custodian').first.present?
-      event = JsonPath.on(composition_resource, '$.event.code.coding.code').first.present?
-      event_code = JsonPath.on(composition_resource, '$.event.code').first.present?
-      event_period = JsonPath.on(composition_resource, '$.event.period').first.present?
-      message_base = "List of Optional Must Support elements populated"
-
-      show_message("#{message_base}: text: #{text}", text)
-      show_message("#{message_base}: identifier: #{identifier}", identifier)
-      show_message("#{message_base}: asserter: #{asserter}", asserter)
-      show_message("#{message_base}: asserter.mode: #{asserter_mode}", asserter_mode)
-      show_message("#{message_base}: asserter.time: #{asserter_time}", asserter_time)
-      show_message("#{message_base}: asserter.party: #{asserter_party}", asserter_party)
-      show_message("#{message_base}: custodian: #{custodian}", custodian)
-      show_message("#{message_base}: event: #{event}", event)
-      show_message("#{message_base}: event.code: #{event_code}", event_code)
-      show_message("#{message_base}: event.period: #{event_period}", event_period)
+      OPTIONAL_MS_ELEMENTS.each do |element|
+        execute_statistics(composition_resource, element[:expression],
+                           "List of Optional Must Support elements populated", element[:label])
+      end
     end
 
     run do
