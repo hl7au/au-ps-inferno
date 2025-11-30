9ce3fcc040424a9950ed2d98ae5999a7f671f9d6
Pavel Rozhkov <prozskov@gmail.com>
2025-11-30 06:19:53 +0100

    Refactor `read_composition_sections_info` in `BasicTestClass`
    - Extract `check_composition_section_code` for improved clarity and modularity.
    - Simplify logic by reducing inline operations and delegating responsibilities to helper methods.

--- lib/au_ps_inferno/utils/basic_test_class.rb
+++ lib/au_ps_inferno/utils/basic_test_class.rb
@@ -175,23 +175,8 @@ module AUPSTestKit
 
     def read_composition_sections_info(sections_array_codes)
       check_bundle_exists_in_scratch
-      au_ps_bundle_resource = BundleDecorator.new(scratch_bundle.to_hash)
-      composition_resource = au_ps_bundle_resource.composition_resource
       sections_array_codes.each do |section_code|
-        section = composition_resource.section_by_code(section_code)
-        if section.nil?
-          warning "Section #{section_code} not found in Composition resource"
-          next
-        end
-        section_references = section.entry_references
-        if section_references.empty?
-          warning "Section #{section.code.coding.first.display}(#{section_code}) has no entries"
-        else
-          section_resources_array = section.entry_references.map do |ref|
-            au_ps_bundle_resource.resource_info_by_entry_full_url(ref)
-          end.join("\n\n")
-          info "**Section #{section.code.coding.first.display}**:\n\n#{section_resources_array}"
-        end
+        check_composition_section_code(section_code, BundleDecorator.new(scratch_bundle.to_hash).composition_resource)
       end
     end
   end
