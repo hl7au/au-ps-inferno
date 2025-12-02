a31d6425fceb3e3df224df2fa9dc79d7e29cb060
Pavel Rozhkov <prozskov@gmail.com>
2025-12-02 11:25:42 +0100

    Refactor `BundleDecorator` to enhance entry resolution and resource handling logic
    - Introduced `composition_entry` method for locating Composition entries.
    - Added `resolve_entry_reference` and `resolve_entry_reference_as_reference` methods for improved entry reference handling.
    - Replaced direct `entry.find` calls with `resolve_entry_reference`.

--- lib/au_ps_inferno/utils/bundle_decorator.rb
+++ lib/au_ps_inferno/utils/bundle_decorator.rb
@@ -17,8 +17,12 @@ class BundleDecorator < FHIR::Bundle
     CompositionDecorator.new(composition_resource_data)
   end
 
+  def composition_entry
+    entry.find { |entr| entr.resource.resourceType == 'Composition' }
+  end
+
   def resource_info_by_entry_full_url(entry_full_url)
-    entry_by_full_url = entry.find { |entr| entr.fullUrl == entry_full_url }
+    entry_by_full_url = resolve_entry_reference(entry_full_url)
     return "Entry with fullUrl #{entry_full_url} not found in Bundle" if entry_by_full_url.nil?
 
     resource = entry_by_full_url.resource
@@ -26,5 +30,31 @@ class BundleDecorator < FHIR::Bundle
     profiles = profiles.length.positive? ? profiles.join(', ') : 'Without Profiles'
 
     "#{resource.resourceType} (#{profiles})"
+  end
+
+  private
+
+  def resolve_entry_reference_as_reference(entry_reference)
+    entry.find do |entr|
+      entry_full_url = entr.fullUrl
+      next unless entry_full_url.start_with?('http') || entry_full_url.start_with?('https')
+
+      url_without_params = entry_full_url.split('?').first
+      base_url = url_without_params.split(entry_reference).first
+
+      entr.fullUrl == base_url + entry_reference
+    end
+  end
+
+  def resolve_entry_reference(entry_reference)
+    is_urn = entry_reference.start_with?('urn:')
+    is_url = entry_reference.start_with?('http') || entry_reference.start_with?('https')
+    is_reference = entry_reference.split('/').length == 2
+
+    return entry.find { |entr| entr.fullUrl == entry_reference } if is_urn || is_url
+
+    resolve_entry_reference_as_reference(entry_reference) if is_reference
+
+    nil
   end
 end
