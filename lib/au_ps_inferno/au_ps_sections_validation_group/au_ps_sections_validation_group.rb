c8e4df93c9500cd64d22f205f63b003e8c88b1e9
Pavel Rozhkov <prozskov@gmail.com>
2026-01-04 18:18:54 +0100

    Add validation test for ADVANCE HEALTHCARE DIRECTIVES section in AU PS
    - Introduced `AUPSSectionsAdvanceDirectivesValidation` to validate the ADVANCE HEALTHCARE DIRECTIVES section in the Composition resource.
    - Updated `AUPSSectionsValidationGroup` to include the new validation test.

--- lib/au_ps_inferno/au_ps_sections_validation_group/au_ps_sections_validation_group.rb
+++ lib/au_ps_inferno/au_ps_sections_validation_group/au_ps_sections_validation_group.rb
@@ -7,6 +7,7 @@ require_relative './au_ps_sections_immunizations_validation'
 require_relative './au_ps_sections_results_validation'
 require_relative './au_ps_sections_procedures_validation'
 require_relative './au_ps_sections_medical_devices_validation'
+require_relative './au_ps_sections_advance_directives_validation'
 require_relative '../utils/constants'
 
 module AUPSTestKit
@@ -26,5 +27,6 @@ module AUPSTestKit
     test from: :au_ps_sections_results_validation
     test from: :au_ps_sections_procedures_validation
     test from: :au_ps_sections_medical_devices_validation
+    test from: :au_ps_sections_advance_directives_validation
   end
 end
