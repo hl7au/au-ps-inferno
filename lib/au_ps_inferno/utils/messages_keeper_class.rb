4e0c7adc48fc0aac1e435d2cdeaba9e97f0827eb
Pavel Rozhkov <prozskov@gmail.com>
2026-01-19 14:55:47 +0100

    Refactor MessagesKeeper to encapsulate rich message hash building in a private method, improving code organization and clarity. Introduce cleanup_message_body method to streamline message formatting by removing resource identifiers from message bodies.

--- lib/au_ps_inferno/utils/messages_keeper_class.rb
+++ lib/au_ps_inferno/utils/messages_keeper_class.rb
@@ -4,6 +4,40 @@ require 'digest'
 
 # A class to keep messages for a section test
 class MessagesKeeper
+  class << self
+    def build_rich_message_hash(resource, idx, profile_url, message)
+      # NOTE: I don't like this method, because we need to pass arguments that are not related to the message itself.
+      # But it's easier to use than to pass a block to the add_message method. So, let's keep it for now.
+      # Pavel Rozhkov, 2026-01-18
+      build_hash(resource, idx, profile_url, message)
+    end
+
+    private
+
+    def build_hash(resource, idx, profile_url, message)
+      message_body = cleanup_message_body(message[:message], resource.resourceType, resource.id)
+      {
+        message: message_body,
+        type: message[:type],
+        resource_type: resource.resourceType,
+        profile: profile_url,
+        resource_id: resource.id,
+        idx: idx,
+        signature: Digest::MD5.hexdigest([resource.resourceType, profile_url, message_body].compact.join('|'))
+      }
+    end
+
+    def cleanup_message_body(message, resource_type, resource_id)
+      # This function is used to remove resourceType/id from the message body.
+      # AllergyIntolerance: AllergyIntolerance.code: None of the ...
+      # AllergyIntolerance/123: AllergyIntolerance.code: None of the ...
+      # should be converted to:
+      # AllergyIntolerance.code: None of the ...
+      string_to_replace = resource_id.present? ? "#{resource_type}/#{resource_id}: " : "#{resource_type}: "
+      message.sub(string_to_replace, '')
+    end
+  end
+
   attr_reader :messages
 
   def initialize
@@ -20,22 +54,6 @@ class MessagesKeeper
 
   def warnings
     messages_of_type('warning')
-  end
-
-  def self.build_rich_message_hash(resource, idx, profile_url, message)
-    # NOTE: I don't like this method, because we need to pass arguments that are not related to the message itself.
-    # But it's easier to use than to pass a block to the add_message method. So, let's keep it for now.
-    # Pavel Rozhkov, 2026-01-18
-    resource_type = resource.resourceType
-    {
-      message: message[:message],
-      type: message[:type],
-      resource_type: resource_type,
-      profile: profile_url,
-      resource_id: resource.id,
-      idx: idx,
-      signature: Digest::MD5.hexdigest([resource_type, profile_url, message[:message]].compact.join('|'))
-    }
   end
 
   private
