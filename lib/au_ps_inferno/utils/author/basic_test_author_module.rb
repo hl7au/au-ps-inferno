e151092d84525e1bf7319cc3ffd35d1c9f2f7c2a
Pavel Rozhkov <prozskov@gmail.com>
2026-04-14 12:23:54 +0200

    Remove unused code
    Ref: https://github.com/hl7au/au-ps-inferno/issues/56

--- lib/au_ps_inferno/utils/author/basic_test_author_module.rb
+++ lib/au_ps_inferno/utils/author/basic_test_author_module.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
 
 require_relative 'basic_test_author_resource'
-require_relative 'basic_test_author_ms_elements_and_slices'
 require_relative 'basic_test_author_tests'
 
 module AUPSTestKit
   # Composes Composition author Must Support helpers for BasicTest.
   module BasicTestAuthorModule
     include BasicTestAuthorResource
-    include BasicTestAuthorMsElementsAndSlices
     include BasicTestAuthorTests
   end
 end
