09c4ed71661d2b0a127aaa7397e1c850316a1dcc
Pavel Rozhkov <prozskov@gmail.com>
2026-07-22 16:50:36 +0400

    Add inferno_core 1.4.0 to provide an ability to use enable_when for inputs

--- lib/au_ps_inferno/utils/common_inputs_module.rb
+++ lib/au_ps_inferno/utils/common_inputs_module.rb
@@ -6,6 +6,27 @@ module AUPSTestKit
   # RetrieveBundleTestClass, GenerateSummaryBundleTestClass), so they aren't redeclared in each.
   module CommonInputsModule
     SINGLE_INPUT_DEFINITIONS = {
+      bundle_retrieve_method_input: [:bundle_retrieve_method, {
+        title: 'Retrieve Bundle method',
+        optional: true,
+        type: 'radio',
+        options: {
+          list_options: [
+            {
+              label: 'JSON input',
+              value: 'json_input'
+            },
+            {
+              label: 'Retrieve Bundle',
+              value: 'retrieve_bundle'
+            },
+            {
+              label: '$summary',
+              value: 'summary_operation'
+            }
+          ]
+        }
+      }],
       validate_against_input: [:validate_against, {
         title: 'Validate Against',
         optional: true,
@@ -25,25 +46,32 @@ module AUPSTestKit
         }
       }],
       bundle_id_input: [:bundle_id, {
-        title: 'Bundle ID', optional: true, description: 'To request Bundle/{bundle_id}'
+        title: 'Bundle ID', optional: true, description: 'To request Bundle/{bundle_id}',
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'retrieve_bundle' }
       }],
       bundle_url_input: [:bundle_url, {
-        title: 'Bundle URL', optional: true, description: 'To retrieve document Bundle using HTTP GET request'
+        title: 'Bundle URL', optional: true, description: 'To retrieve document Bundle using HTTP GET request',
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'retrieve_bundle' }
       }],
       bundle_resource_input: [:bundle_resource, {
-        title: 'Bundle Resource', optional: true, description: 'If you want to check existing Bundle resource', type: 'textarea'
+        title: 'Bundle Resource', optional: true,
+        description: 'If you want to check existing Bundle resource', type: 'textarea',
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'json_input' }
       }],
       patient_id_input: [:patient_id, {
-        title: 'Patient ID', optional: true, description: 'To request Patient/{patient_id}/$summary'
+        title: 'Patient ID', optional: true, description: 'To request Patient/{patient_id}/$summary',
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'summary_operation' }
       }],
       patient_identifier_input: [:identifier, {
-        title: 'Patient Identifier', optional: true, description: 'To request Patient/$summary?identifier={identifier}'
+        title: 'Patient Identifier', optional: true, description: 'To request Patient/$summary?identifier={identifier}',
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'summary_operation' }
       }],
       profile_input: [:profile, {
         title: 'Profile URL',
         optional: true,
         default: 'http://hl7.org.au/fhir/ps/StructureDefinition/au-ps-bundle',
-        description: 'To specify profile for the patient summary'
+        description: 'To specify profile for the patient summary',
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'summary_operation' }
       }],
       credentials_input: [:credentials, {
         title: 'OAuth Credentials', type: :oauth_credentials, optional: true
@@ -65,14 +93,10 @@ module AUPSTestKit
       end
     end
 
-    def self.auth_header_input(klass)
-      header_name_input(klass)
-      header_value_input(klass)
-    end
-
     def self.authentication_inputs(klass)
       credentials_input(klass)
-      auth_header_input(klass)
+      header_name_input(klass)
+      header_value_input(klass)
     end
 
     INPUT_NAMES_BY_METHOD = SINGLE_INPUT_DEFINITIONS.transform_values { |(input_name, _)| [input_name] }.merge(
@@ -86,12 +110,12 @@ module AUPSTestKit
     end
 
     def self.bundle_resource_inputs(klass)
-      declare_inputs(klass, :validate_against_input, :bundle_resource_input)
+      declare_inputs(klass, :validate_against_input, :bundle_retrieve_method_input, :bundle_resource_input)
     end
 
     def self.retrieve_bundle_inputs(klass)
-      declare_inputs(klass, :bundle_url_input, :fhir_server_url_input, :bundle_id_input,
-                     :authentication_inputs, :validate_against_input)
+      declare_inputs(klass, :validate_against_input, :bundle_url_input, :fhir_server_url_input, :bundle_id_input,
+                     :authentication_inputs)
 
       configure_fhir_client(klass)
     end
