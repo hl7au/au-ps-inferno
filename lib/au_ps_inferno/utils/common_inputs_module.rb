1f745ef0a4cdfe36eed7f2a55c2778f0acf36088
Pavel Rozhkov <prozskov@gmail.com>
2026-07-23 19:13:12 +0400

    Update input definitions in CommonInputsModule for FHIR server integration

--- lib/au_ps_inferno/utils/common_inputs_module.rb
+++ lib/au_ps_inferno/utils/common_inputs_module.rb
@@ -21,8 +21,8 @@ module AUPSTestKit
               value: 'retrieve_bundle'
             },
             {
-              label: '$summary',
-              value: 'summary_operation'
+              label: 'FHIR Server',
+              value: 'fhir_server'
             }
           ]
         }
@@ -47,7 +47,7 @@ module AUPSTestKit
       }],
       bundle_id_input: [:bundle_id, {
         title: 'Bundle ID', optional: true, description: 'To request Bundle/{bundle_id}',
-        enable_when: { input_name: 'bundle_retrieve_method', value: 'retrieve_bundle' }
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'fhir_server' }
       }],
       bundle_url_input: [:bundle_url, {
         title: 'Bundle URL', optional: true, description: 'To retrieve document Bundle using HTTP GET request',
@@ -60,32 +60,55 @@ module AUPSTestKit
       }],
       patient_id_input: [:patient_id, {
         title: 'Patient ID', optional: true, description: 'To request Patient/{patient_id}/$summary',
-        enable_when: { input_name: 'bundle_retrieve_method', value: 'summary_operation' }
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'fhir_server' }
       }],
       patient_identifier_input: [:identifier, {
         title: 'Patient Identifier', optional: true, description: 'To request Patient/$summary?identifier={identifier}',
-        enable_when: { input_name: 'bundle_retrieve_method', value: 'summary_operation' }
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'fhir_server' }
       }],
       profile_input: [:profile, {
         title: 'Profile URL',
         optional: true,
         default: 'http://hl7.org.au/fhir/ps/StructureDefinition/au-ps-bundle',
         description: 'To specify profile for the patient summary',
-        enable_when: { input_name: 'bundle_retrieve_method', value: 'summary_operation' }
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'fhir_server' }
       }],
       credentials_input: [:credentials, {
-        title: 'OAuth Credentials', type: :oauth_credentials, optional: true
+        title: 'OAuth Credentials', type: :oauth_credentials, optional: true,
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'fhir_server' }
       }],
       fhir_server_url_input: [:url, {
-        title: 'FHIR Server Base Url', optional: true
+        title: 'FHIR Server Base Url', optional: true,
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'fhir_server' }
       }],
       header_name_input: [:header_name, {
-        title: 'Header name', optional: true
+        title: 'Header name', optional: true,
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'fhir_server' }
+      }],
+      header_name_retrieve_input: [:header_name, {
+        title: 'Header name', optional: true,
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'retrieve_bundle' }
       }],
       header_value_input: [:header_value, {
-        title: 'Header value', optional: true
+        title: 'Header value', optional: true,
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'fhir_server' }
+      }],
+      header_value_retrieve_input: [:header_value, {
+        title: 'Header value', optional: true,
+        enable_when: { input_name: 'bundle_retrieve_method', value: 'retrieve_bundle' }
       }]
     }.freeze
+
+    BUNDLE_RESOURCE_INPUTS_DEFINITION = %i[validate_against_input bundle_retrieve_method_input
+                                           bundle_resource_input].freeze
+    RETRIEVE_BUNDLE_INPUTS_DEFINITION = %i[validate_against_input bundle_retrieve_method_input
+                                           bundle_url_input
+                                           bundle_id_input header_name_retrieve_input header_value_retrieve_input].freeze
+    FHIR_SERVER_INPUTS_DEFINITION = %i[validate_against_input bundle_retrieve_method_input fhir_server_url_input
+                                       patient_id_input patient_identifier_input
+                                       profile_input bundle_id_input credentials_input header_name_input
+                                       header_value_input].freeze
+    RETRIEVE_CS_INPUTS_DEFINITION = %i[].freeze
 
     SINGLE_INPUT_DEFINITIONS.each do |method_name, (input_name, options)|
       define_singleton_method(method_name) do |klass|
@@ -93,16 +116,7 @@ module AUPSTestKit
       end
     end
 
-    def self.authentication_inputs(klass)
-      credentials_input(klass)
-      header_name_input(klass)
-      header_value_input(klass)
-    end
-
-    INPUT_NAMES_BY_METHOD = SINGLE_INPUT_DEFINITIONS.transform_values { |(input_name, _)| [input_name] }.merge(
-      validate_against_input: [:validate_against],
-      authentication_inputs: %i[credentials header_name header_value]
-    ).freeze
+    INPUT_NAMES_BY_METHOD = SINGLE_INPUT_DEFINITIONS.transform_values { |(input_name, _)| [input_name] }.freeze
 
     def self.declare_inputs(klass, *method_names)
       klass.input_order(*method_names.flat_map { |method_name| INPUT_NAMES_BY_METHOD.fetch(method_name) })
@@ -110,25 +124,22 @@ module AUPSTestKit
     end
 
     def self.bundle_resource_inputs(klass)
-      declare_inputs(klass, :validate_against_input, :bundle_retrieve_method_input, :bundle_resource_input)
+      declare_inputs(klass, *BUNDLE_RESOURCE_INPUTS_DEFINITION)
     end
 
     def self.retrieve_bundle_inputs(klass)
-      declare_inputs(klass, :validate_against_input, :bundle_url_input, :fhir_server_url_input, :bundle_id_input,
-                     :authentication_inputs)
-
+      declare_inputs(klass, *RETRIEVE_BUNDLE_INPUTS_DEFINITION)
       configure_fhir_client(klass)
     end
 
     def self.summary_inputs(klass)
-      declare_inputs(klass, :fhir_server_url_input, :patient_id_input, :patient_identifier_input,
-                     :profile_input, :authentication_inputs, :validate_against_input)
+      declare_inputs(klass, *FHIR_SERVER_INPUTS_DEFINITION)
 
       configure_fhir_client(klass)
     end
 
     def self.retrieve_cs_inputs(klass)
-      declare_inputs(klass, :fhir_server_url_input, :authentication_inputs)
+      declare_inputs(klass, *RETRIEVE_CS_INPUTS_DEFINITION)
 
       configure_fhir_client(klass)
     end
