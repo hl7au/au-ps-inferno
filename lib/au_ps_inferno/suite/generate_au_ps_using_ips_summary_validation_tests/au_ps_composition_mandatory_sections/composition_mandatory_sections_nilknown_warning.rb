675e1a06a80513b297f94b6e5697fb5c40349e00
Pavel Rozhkov <prozskov@gmail.com>
2026-08-05 16:08:16 +0400

    Add section negation checks and related tests
    Ref: https://github.com/hl7au/au-ps-inferno/issues/34

---
+++ lib/au_ps_inferno/suite/generate_au_ps_using_ips_summary_validation_tests/au_ps_composition_mandatory_sections/composition_mandatory_sections_nilknown_warning.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative '../../../utils/basic_test_class'
+
+module AUPSTestKit
+  # Warns against using emptyReason=nilknown instead of an explicit negation entry
+  class AUPSSuiteGenerateAuPsUsingIpsSummaryValidationTestsAuPsCompositionMandatorySectionsNilknownWarning < BasicTest
+    title 'AU PS Composition Mandatory Sections do not rely on emptyReason=nilknown'
+    description 'Warns when a mandatory section (Problems, Allergies, Medications) uses ' \
+                 'Composition.section.emptyReason = nilknown instead of an explicit negation code on the ' \
+                 "section's entry resource (e.g. AllergyIntolerance.code = 716186003 |No known allergy|). " \
+                 'AU PS prefers the explicit-entry pattern used by FHIR, IPS and AU Core over emptyReason ' \
+                 '- this is a warning, not a failure.'
+    id :suite_generate_au_ps_using_ips_summary_validation_tests_au_ps_composition_mandatory_sections_nilknown_warning
+
+    run do
+      warn_on_nilknown_empty_reason(%w[11450-4 48765-2 10160-0])
+    end
+  end
+end
--- lib/au_ps_inferno/suite/retrieve_au_ps_bundle_validation_tests/au_ps_composition_mandatory_sections.rb
+++ lib/au_ps_inferno/suite/retrieve_au_ps_bundle_validation_tests/au_ps_composition_mandatory_sections.rb
@@ -4,6 +4,8 @@ require_relative 'au_ps_composition_mandatory_sections/composition_mandatory_sec
 
 require_relative 'au_ps_composition_mandatory_sections/composition_mandatory_sections_mandatory_sections_entry_profiles'
 
+require_relative 'au_ps_composition_mandatory_sections/composition_mandatory_sections_nilknown_warning'
+
 module AUPSTestKit
   # Automatically generated primitive group for AU PS Composition Mandatory Sections
   class AUPSSuiteRetrieveAuPsBundleValidationTestsAuPsCompositionMandatorySections < Inferno::TestGroup
@@ -16,5 +18,7 @@ module AUPSTestKit
     test from: :suite_retrieve_au_ps_bundle_validation_tests_au_ps_composition_mandatory_sections_sections_shall_populated
 
     test from: :suite_retrieve_au_ps_bundle_validation_tests_au_ps_composition_mandatory_sections_mandatory_sections_entry_profiles
+
+    test from: :suite_retrieve_au_ps_bundle_validation_tests_au_ps_composition_mandatory_sections_nilknown_warning
   end
 end
