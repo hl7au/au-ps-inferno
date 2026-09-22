5891ea1c410d1e7a84c7b380a66e761338ce969d
Pavel Rozhkov <prozskov@gmail.com>
2026-09-22 15:29:29 +0400

    Modal window enchancements (#121)
    Modal window enchancements
    Ref: https://github.com/hl7au/au-ps-inferno/issues/86

--- lib/au_ps_inferno/suite/retrieve_au_ps_bundle_validation_tests/retrieve_au_ps_bundle_validation_tests.rb
+++ lib/au_ps_inferno/suite/retrieve_au_ps_bundle_validation_tests/retrieve_au_ps_bundle_validation_tests.rb
@@ -33,6 +33,8 @@ module AUPSTestKit
 
     run_as_group
 
+    input_order :validate_against
+
     group from: :suite_retrieve_au_ps_bundle_validation_tests_bundle_acquisition
 
     group from: :suite_retrieve_au_ps_bundle_validation_tests_bundle_validation
